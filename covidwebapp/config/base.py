a624357e23f0e0f9ba529760a8e2527cd209c9c9
McIntyre-Colin <colinhmcintyre@gmail.com>
2022-07-05 23:11:02 -0400

    Adding static folder to config

--- covidwebapp/config/base.py
+++ covidwebapp/config/base.py
@@ -116,7 +116,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/core/static/'
+STATIC_URL = '/static/'
 
 # We also needed to add this to specify where the downloads go
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')
